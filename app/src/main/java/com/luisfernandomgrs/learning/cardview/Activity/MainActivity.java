f77fff0e0deeafe34544d81693a1536c71cd5e99
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-05-31 23:23:40 -0300

    full basic implementation

--- app/src/main/java/com/luisfernandomgrs/learning/cardview/Activity/MainActivity.java
+++ app/src/main/java/com/luisfernandomgrs/learning/cardview/Activity/MainActivity.java
@@ -1,31 +1,77 @@
 package com.luisfernandomgrs.learning.cardview.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.luisfernandomgrs.learning.cardview.Adapter.PostagemAdapter;
+import com.luisfernandomgrs.learning.cardview.Model.Postagem;
 import com.luisfernandomgrs.learning.cardview.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Postagem> postagens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle("Visualizando minhas fotos");
 
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerPostagem);
 
         // Define layout
+        /*
+        // Criando um layout convencional...
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        */
+        /*
+        // Criando um layout horizontal
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        */
+        /*
+        // Criando um layout de tabela com duas colunas...
+        */
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
 
         // Define Adapter
-        PostagemAdapter adapter = new PostagemAdapter();
+        this.prepararPostagens();
+        PostagemAdapter adapter = new PostagemAdapter(postagens);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void prepararPostagens() {
+        Postagem postedBy;
+
+        postedBy = new Postagem("Luis Fernando Machado", "Janeiro de 2022, 12", R.drawable.imagem1);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Luis Fernando", "Janeiro de 2022, 17", R.drawable.imagem2);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Fernando", "Janeiro de 2022, 19", R.drawable.imagem3);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Fernando", "Janeiro de 2022, 22", R.drawable.imagem2);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Luis Fernando", "Fevereiro de 2022, 7", R.drawable.imagem3);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Luis", "Fevereiro de 2022, 11", R.drawable.imagem3);
+        this.postagens.add(postedBy);
+
+        postedBy = new Postagem("Luis", "Fevereiro de 2022, 7", R.drawable.imagem4);
+        this.postagens.add(postedBy);
     }
 }
--- app/src/main/java/com/luisfernandomgrs/learning/cardview/Adapter/PostagemAdapter.java
+++ app/src/main/java/com/luisfernandomgrs/learning/cardview/Adapter/PostagemAdapter.java
@@ -1,5 +1,6 @@
 package com.luisfernandomgrs.learning.cardview.Adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,30 +9,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.luisfernandomgrs.learning.cardview.Model.Postagem;
+import com.luisfernandomgrs.learning.cardview.R;
+
+import java.util.List;
+
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
+    List<Postagem> listaPostagens;
+
+    public PostagemAdapter(List<Postagem> _listaPostagens) {
+        this.listaPostagens = _listaPostagens;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.postagem_detalhe, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        Postagem _postagem = listaPostagens.get(position);
+        holder.textNome.setText(_postagem.getNome());
+        holder.textPostagem.setText(_postagem.getPostagem());
+        holder.imagemPostagem.setImageResource(_postagem.getImagem());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.listaPostagens.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imagemPostagem;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            textNome = itemView.findViewById(R.id.textNome);
+            textPostagem = itemView.findViewById(R.id.textPostagem);
+            imagemPostagem = itemView.findViewById(R.id.imagePostagem);
         }
     }
 }
